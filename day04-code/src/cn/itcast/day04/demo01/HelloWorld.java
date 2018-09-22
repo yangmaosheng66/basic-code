33302c4629ecb9f77df44fc435fb7beae783a748
yangmaosheng66 <et4545@126.com>
2018-09-22 13:18:16 +0800

    first

--- day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
+++ day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -5,3 +5,17 @@ public class HelloWorld {
         System.out.println("Hello World!");
     }
 }
+
+/*
+IDEA常用快捷键
+
+Alt+Enter               导入包，自动修正代码
+Ctrl+Y                  删除光标所在行
+Ctrl+D                  复制光标所在行的内容，插入光标位置下面
+Ctrl+Alt+L              格式化代码
+Ctrl+/                  单行注释，再按取消注释
+Ctrl+Shift+/            选中代码注释，多行注释，再按取消注释
+Alt+Ins                 自动生成代码，toString、get、set等方法
+Alt+Shift+上下箭头      移动当前代码行
+5.fori                 循环5次的for循环语句
+ */
