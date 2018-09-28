5b200cd147a8ea12345e438fef70dda6011f7c0e
yangmaosheng66 <et4545@126.com>
2018-09-28 16:26:59 +0800

    first

--- day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
+++ day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -18,6 +18,7 @@ Ctrl+Shift+/            选中代码注释，多行注释，再按取消注释
 Alt+Ins                 自动生成代码，toString、get、set等方法
 Alt+Shift+上下箭头      移动当前代码行
 5.fori                 循环5次的for循环语句
+array.fori              用for循环对数组array进行遍历
 选中后shift + F6        相同内容批量更改
 ctrl + 左键方法名        查看该方法的定义或调用
  */
